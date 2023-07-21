93b72541f8bc0c9d4ae3edbdc05e9a30d02e8d0d
Pipont <89101672+Pipont@users.noreply.github.com>
2023-07-21 15:58:53 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/App.jsx
+++ src/App.jsx
@@ -24,18 +24,18 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Pedro Soto",
+  title: "Innovative student and developer",
+  email: "pedrosotobarboza@hotmail.com ",
+  gitHub: "Pipont",
+  instagram: "",
+  linkedIn: "pedrosotobarboza",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
-const primaryColor = "#4E567E";
+const primaryColor = "#0D3B66";
 const secondaryColor = "#D2F1E4";
 
 const App = () => {
